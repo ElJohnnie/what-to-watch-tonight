28ddcf0b0f6ad884e7494577c31b0409e36b2bf2
Jonatha <jonatha.follmer@telefonicati.onmicrosoft.com>
2024-07-30 14:11:15 -0300

    Jonatha: Atualização por script dos submódulos

--- submodules-update.sh
+++ submodules-update.sh
@@ -3,9 +3,15 @@
 GIT_USER=$(git config user.name)
 
 git submodule init
-git submodule update --remote --merge
+
+git submodule update --remote --merge wtwt-back-end
+git submodule update --remote --merge wtwt-front-end
+git submodule update --remote --merge wtwt-ml-model
+
+git add wtwt-back-end wtwt-front-end wtwt-ml-model
 
 git add .
+
 git commit -m "$GIT_USER: Atualização por script dos submódulos"
 
 git push origin main
